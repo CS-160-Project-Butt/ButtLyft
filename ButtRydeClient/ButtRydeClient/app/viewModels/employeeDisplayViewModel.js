ef1c058f75e30566b212708a26e4b538f71b43a3
jelifish <jeffmunkee@gmail.com>
2016-09-20 18:17:17 -0700

    Revert "viewmodel and view trimming"
    This reverts commit ec01a71f9a22dc380f222e24907fb7e97c127be4.

---
+++ ButtRydeClient/ButtRydeClient/app/viewModels/employeeDisplayViewModel.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+app.factory('employeeDisplayViewModel', function () {
+    return kendo.data.Model.define({
+        id: 'id',
+        fields: {
+            id: { type: 'string', editable: false, nullable: false },
+            jobTitle: { type: 'string', editable: false, nullable: true },
+            applicationUserId: { type: 'string', editable: false, nullable: false, defaultValue: '00000000-0000-0000-0000-000000000000' },
+            username: { type: 'string', editable: false, nullable: false },
+            firstName: { type: 'string', editable: false, nullable: false },
+            lastName: { type: 'string', editable: false, nullable: false },
+            email: { type: 'string', editable: false, nullable: false }
+        }
+    });
+});
