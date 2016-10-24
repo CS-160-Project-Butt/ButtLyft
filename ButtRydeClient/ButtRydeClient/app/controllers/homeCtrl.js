037743309dfd51bd8c54fc8515cb9f2ad6622894
jelifish <jeffmunkee@gmail.com>
2016-10-24 03:31:02 -0700

    documentation
    provided documentation for functions.

--- ButtRydeClient/ButtRydeClient/app/controllers/homeCtrl.js
+++ ButtRydeClient/ButtRydeClient/app/controllers/homeCtrl.js
@@ -2,12 +2,29 @@
 app.controller('homeCtrl', ['$timeout','$location', 'authService', 'NgMap', function ($timeout,$location, authService, NgMap) {
     var vm = this;
 
-    vm.map = null;
-    vm.startAddress = [0, 0];
-    vm.inputAddress = null;
-    vm.mapCenter = [0, 0];
-    vm.dragging = false;
+    vm.map = null; //this is the object that ngmap gives us
+    vm.startAddress = [0, 0]; //this is used to store the location of the rider's pickup point
+    vm.endAddress = [0, 0]; // this is the location of the rider's destination
+    vm.inputAddress = null; //user types in the textbox and queries where to go, then hits enter or presses search
+    vm.mapCenter = [0, 0]; //position of the center of the map.
+    vm.dragging = false; //bool: if user is done dragging the map, expand the marker
 
+    NgMap.getMap().then(function (map) { //this could be used as an initialize function
+        vm.map = map; //we get the map
+        console.log(vm.startAddress);
+        console.log(map); //checking out the map
+        console.log('markers', map.markers);
+        console.log('shapes', map.shapes);
+        vm.onCenterChanged();//initialize the position of the center marker
+    });
+
+    /**
+     * Used by the html map to alter/update the map location
+     * when we start program, map center initialized to geolocation
+     * after that its wherever the user drags to(number array pair), or the typed in address(string)
+     * after the center is changed we have to update the position of the center marker
+     * parameter: location - usually is null, if not null then force map position to be &location, update center marker
+     */
     vm.processLocation = function (location) {
         if (vm.mapCenter[0] == 0 && vm.mapCenter[1] == 0) {
             navigator.geolocation.getCurrentPosition(function (position) {
@@ -24,20 +41,18 @@ app.controller('homeCtrl', ['$timeout','$location', 'authService', 'NgMap', func
         return vm.mapCenter;
     }
 
-    NgMap.getMap().then(function (map) { //this could be used as an initialize function
-        vm.map = map;
-        //vm.startAddress[0] = vm.map.getCenter().lat();
-        //vm.startAddress[1] = vm.map.getCenter().lng();
-        console.log(vm.startAddress);
-        console.log(map);
-        console.log('markers', map.markers);
-        console.log('shapes', map.shapes);
-        vm.onCenterChanged();//initialize center
-    });
-
+    /**
+     * when we are done dragging the map or if idle we have to expand the little box thing
+     */
     vm.onDragEnd = function () {
         vm.dragging = false;
     }
+
+    /**
+     * this function is usually called when the user drags on the map
+     * it will sync the location of the center marker to the map screen center
+     * parameter: center, usually null, if not null then we will set the pos of marker to the argument(&center) instead
+     */
     vm.onCenterChanged = function (center) {
         vm.dragging = true;
         if (vm.map != null) {
@@ -49,6 +64,9 @@ app.controller('homeCtrl', ['$timeout','$location', 'authService', 'NgMap', func
         }
     }
 
+    /**
+     * once we figure out where we want to get picked up from we click the button and the button calls this and does things
+     */
     vm.setStartAddress = function () {
         vm.startAddress = vm.mapCenter;
         console.log("your starting address is" + vm.mapCenter);
