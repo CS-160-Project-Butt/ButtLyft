a75ffb370fa5c86a95761a7bc8dc51749fc70553
jelifish <jeffmunkee@gmail.com>
2016-10-27 13:27:30 -0700

    fixed signalr bug

--- ButtRydeClient/ButtRydeClient/app/services/signalService.js
+++ ButtRydeClient/ButtRydeClient/app/services/signalService.js
@@ -2,10 +2,12 @@
 app.factory('signalService', ['$', function ($) {
 
     var hub = null;
+    var proxy;
+    var connection;
 
     return {
         initialize: function () {
-            connection = $.hubConnection("/dataHub", { useDefaultPath: false });
+            connection = $.hubConnection();
 
             this.hub = connection.createHubProxy('dataHub');
 
